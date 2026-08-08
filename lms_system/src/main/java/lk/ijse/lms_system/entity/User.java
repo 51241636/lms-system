b79ed7482803f6374e40090945f1ffc918ecaf02
51241636 <pisepala@gmail.com>
2026-08-08 12:08:34 +0530

    first commit

---
+++ lms_system/src/main/java/lk/ijse/lms_system/entity/User.java
@@ -0,0 +1,32 @@
+package lk.ijse.lms_system.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    @Column(unique = true,nullable = false,length = 50)
+    private String username;
+    @Column(length = 50,nullable = false)
+    private String password;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email address")
+    private String email;
+    @Pattern(
+            regexp = "^07[0-9]{8}$",
+            message = "Invalid Sri Lankan contact number"
+    )
+    private String contact;
+
+}
