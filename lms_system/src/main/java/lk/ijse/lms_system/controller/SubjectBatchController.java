99b73f2e3c42013376498e956d9b1fbc26cb0825
51241636 <pisepala@gmail.com>
2026-08-10 22:52:38 +0530

    subjectBatch controller class updation

--- lms_system/src/main/java/lk/ijse/lms_system/controller/SubjectBatchController.java
+++ lms_system/src/main/java/lk/ijse/lms_system/controller/SubjectBatchController.java
@@ -1,4 +1,36 @@
 package lk.ijse.lms_system.controller;
 
+import jakarta.validation.Valid;
+import lk.ijse.lms_system.constant.CommonResponse;
+import lk.ijse.lms_system.dto.StudentDTO;
+import lk.ijse.lms_system.dto.SubjectBatchDTO;
+import lk.ijse.lms_system.service.SubjectBatchService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import static lk.ijse.lms_system.constant.ResponseCode.OPERATION_SUCCSESS;
+import static lk.ijse.lms_system.constant.ResponseMessage.RESPONSE_MESSAGE;
+
+@RestController
+@RequestMapping(value = "v1/subjectBatch")
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin
 public class SubjectBatchController {
+    private final SubjectBatchService subjectBatchService;
+
+
+    //    add subjectBatch
+    @PreAuthorize("hasAnyRole('Admin','Teacher')")
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse addSubjectBatch(@Valid @RequestBody SubjectBatchDTO subjectBatchDTO) {
+        log.info("get subjectBatch details");
+        subjectBatchService.addSubjectBatch(subjectBatchDTO);
+        log.info("add subjectBatches success");
+        return new CommonResponse(OPERATION_SUCCSESS,RESPONSE_MESSAGE);
+    }
+
 }
